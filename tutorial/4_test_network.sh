7f9c645682e5e95f73e5630a74dbd044c0526fb0
larskuenkel <lars.kuenkel@uni-bielefeld.de>
2022-05-04 19:04:32 +0200

    Changed set names.

--- tutorial/4_test_network.sh
+++ tutorial/4_test_network.sh
@@ -1,2 +1,2 @@
 cd ../deeppulsarnet/
-python create_model_prediction.py -m test_model.pt -f noiseset_pmps_sample_test.csv 
+python create_model_prediction.py -m test_model.pt -f pmps_sample_test.csv 
